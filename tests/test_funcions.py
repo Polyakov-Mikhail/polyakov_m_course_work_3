4c76e2e629673b5b03ff69ec494f93f4b89234f0
Mikhail Polyakov <timadgi@gmail.com>
2024-06-02 15:20:16 +0500

    add tests

---
+++ tests/test_funcions.py
@@ -0,0 +1,13 @@
+from src.functions import data_json, executed_state_data, last_five_operation
+
+
+def test_data_json():
+    pass
+
+
+def test_executed_state_data():
+    pass
+
+
+def test_last_five_operation():
+    pass
