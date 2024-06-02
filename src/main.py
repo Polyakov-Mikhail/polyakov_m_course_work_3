c1c3a15213e41a673a9b37c8e74f43a91e9d400b
Mikhail Polyakov <timadgi@gmail.com>
2024-06-02 19:19:34 +0500

    add main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from functions import data_json, date_formatting_operation, hide_and_split
+
+
 def main():
-    pass
+    last_five_operations = data_json()
+    for operation in last_five_operations:
+        # print(operation)
+        print(f"{date_formatting_operation(operation['date'])} {operation['description']}")
+        if 'from' in operation:
+            print(f"{hide_and_split(operation['from'])} -> {hide_and_split(operation['to'])}")
+            print(f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}\n")
+        else:
+            print(f"{hide_and_split(operation['to'])}")
+            print(f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}\n")
 
 
 if __name__ == "__main__":
