cee4d448b1215604471bdf4ab76a46b1739a65be
Mikhail Polyakov <timadgi@gmail.com>
2024-06-02 15:20:06 +0500

    add functions executed_state_data and last_five_operation

--- src/functions.py
+++ src/functions.py
@@ -2,10 +2,35 @@ import json
 
 
 def data_json():
-    with open("../operations.json") as file:
+    """Функция получает все операции из файла json"""
+    with open("../operations.json", encoding='utf-8') as file:
         data = json.loads(file.read())
 
     return data
 
 
-print(data_json())
+def executed_state_data():
+    """Функция получает только выполненные операции (EXECUTED)"""
+    executed_states = []
+    data = data_json()
+    for executed in data:
+        if executed == {}:
+            continue
+        else:
+            if executed['state'] == 'EXECUTED':
+                executed_states.append(executed)
+
+    return executed_states
+
+
+def last_five_operation():
+    """Функция получает последние 5 операций"""
+    all_operation = executed_state_data()
+    sorted_operations = sorted(all_operation, key=lambda x: x.get('date', '0'))
+    last_operation = sorted_operations[-5:]
+    return last_operation
+
+
+# a = last_five_operation()
+# for b in a:
+#     print(b)
